647c828b7d92d1b7e52deeef1f4624c4a6c0c6e9
unknown <hamzamoustadraf@gmail.com>
2022-06-28 15:58:36 +0100

    first commit

---
+++ src/main/java/com/gstock/gstockApp/GstockAppApplication.java
@@ -0,0 +1,13 @@
+package com.gstock.gstockApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstockAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GstockAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gstock/gstockApp/GstockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gstock.gstockApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GstockAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
